db2bf834b9c66f27da772df69b7ffebff38d6496
AakashTakale <aksh_takale@yahoo.co.in>
2015-04-06 23:11:01 -0700

    Assignment 7 short URL- Long URL

---
+++ app.js
@@ -0,0 +1,88 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var redis = require('redis');
+var bodyParser = require('body-parser');
+var app = express();
+
+client = redis.createClient();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+http.createServer(app).listen(3000);
+
+client.on("error", function(err) {
+    console.log("Error" + err);
+});
+
+var longurl;
+
+var islongUrlExist = function(longurl, callback) {
+    client.hget("sort1", "longurl", function(err, response) {
+        callback(err, response);
+    });
+};
+
+function generateSortUrl() {
+    var temp = Date.now();
+    return (temp.toString(36));
+
+}
+app.post('/getUrl', function(req, res) {
+    var url = req.body.url1;
+    var index = url.indexOf("localhost:3000");
+    if (index > -1 && index < 8) {
+        client.hget(url, "longurl", function(err, response) {
+            res.json({
+                "url": response
+            });
+
+        });
+    } else {
+        
+        client.hget(url, "sorturl", function(err, response) {
+            if (response === null) {
+                var sorturl = generateSortUrl();
+                sorturl = "localhost:3000/" + sorturl;
+                client.hset(url, "sorturl", sorturl, redis.print);
+                client.hset(sorturl, "longurl", url, redis.print);
+                client.zincrby("views", 1, sorturl, redis.print);
+                res.json({
+                    "url": sorturl
+                });
+            } else {
+                    res.json({
+                    "url": response
+                });
+            }
+        });
+
+    }
+
+
+});
+app.get('/gettop', function(req, res) {
+
+    client.zrevrangebyscore("views", "+inf", 0, "limit", 0, 9, function(err, response) {
+        res.json({
+            "top": response
+        });
+    });
+
+});
+app.get('/:url', function(req, res) {
+    var url = req.params.url;
+    url = "localhost:3000/" + url;
+    client.hget(url, "longurl", function(err, response) {
+        if (response === null) {
+            res.status(404).send("Url not exist");
+        } else {
+            client.zincrby("views", 1, url, redis.print);
+            res.redirect(response);
+        }
+    });
+
+});
